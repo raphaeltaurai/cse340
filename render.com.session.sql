11c8ec703ab3a9aa10ffc61db03c1919ae6f0338
raphaeltaurai <shawntaurai@byui.edu>
2025-02-10 17:24:11 +0200

    .

--- render.com.session.sql
+++ render.com.session.sql
@@ -2,7 +2,7 @@
 
 -- DROP TYPE IF EXISTS public.account_type;
 
-CREATE TYPE public.account_type AS ENUM
+CREATE TYPE public.account_type AS ENUM;
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
