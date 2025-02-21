9bc56e563d0af7e938732a475b605dd4a85084fe
raphaeltaurai <shawntaurai@byui.edu>
2025-02-21 21:53:52 +0200

    process reg

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const inventoryRoute = require("./routes/inventoryRoute")
 const accountController = require("./controllers/accountController.js")
 const accountRoute = require("./routes/accountRoute.js")
 const utilities = require("./utilities/")
+const bodyParser = require("body-parser")
 
 /* ***********************
  * Middleware
@@ -39,6 +40,9 @@ app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * View Engine and Templates
